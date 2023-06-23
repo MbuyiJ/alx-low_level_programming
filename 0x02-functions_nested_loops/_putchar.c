9eb2aafde73ace5719bbb26bd6ec9c87adb3e4bc
MbuyiJ <mjtotana@gmail.com>
2023-06-23 10:59:16 -0700

    print_alphabet_x10

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,9 +4,9 @@
  * _putchar - writes the character c to stdout
  * @ c: the character to print
  * Return:On success 1
- * On error: -2 returned, and errno is set properly
+ * On error: -2 returned and errno is set properly
  */
-int _putchar(char C)
+int _putchar(char c)
 {
 	return (write(1, &c,1));
 }
